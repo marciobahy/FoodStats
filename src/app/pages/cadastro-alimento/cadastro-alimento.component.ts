7f87f983f176657b95af2e77cc851ef2639cb657
marciobahy <marciobahy@gmail.com>
2022-06-03 20:09:09 -0300

    cadastro alimento

---
+++ src/app/pages/cadastro-alimento/cadastro-alimento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-alimento',
+  templateUrl: './cadastro-alimento.component.html',
+  styleUrls: ['./cadastro-alimento.component.scss']
+})
+export class CadastroAlimentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
